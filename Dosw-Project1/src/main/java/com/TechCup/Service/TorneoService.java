02404b890fe475a9bf51f567e2be53ea05d9cfba
Maria Juliana Rodriguez Caicedo <majuliroca@gmail.com>
2026-03-13 17:40:11 -0500

    Se agrega esqueleto y tests

---
+++ Dosw-Project1/src/main/java/com/TechCup/Service/TorneoService.java
@@ -0,0 +1,16 @@
+package com.TechCup.service;
+
+import com.TechCup.model.Torneo;
+import java.time.LocalDate;
+
+public class TorneoService {
+
+    public Torneo crearTorneo(String nombre, LocalDate fechaInicio, LocalDate fechaFin,
+                              int cantidadEquipos, double costoPorEquipo) {
+        return null; // TODO
+    }
+
+    public void activarTorneo(Torneo torneo) {
+        // TODO
+    }
+}
---
+++ Dosw-Project1/src/main/java/com/TechCup/Service/UsuarioService.java
@@ -0,0 +1,19 @@
+package com.TechCup.service;
+
+import com.TechCup.model.PerfilDeportivo;
+import com.TechCup.model.Usuario;
+
+public class UsuarioService {
+
+    public Usuario registrarUsuario(String nombre, String correo, String contrasena, String rol) {
+        return null; // TODO
+    }
+
+    public String autenticar(String correo, String contrasena) {
+        return null; // TODO
+    }
+
+    public PerfilDeportivo crearPerfilDeportivo(Usuario usuario, String posicion, int dorsal, boolean disponible) {
+        return null; // TODO
+    }
+}
