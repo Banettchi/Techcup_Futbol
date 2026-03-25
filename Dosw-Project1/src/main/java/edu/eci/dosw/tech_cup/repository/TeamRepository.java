8efdba8fb5de791ad85cc8b806db4333d7665697
JuliRodC <majuliroca@gmail.com>
2026-03-25 15:43:33 -0500

    Se hizo paso 6

--- Dosw-Project1/src/main/java/edu/eci/dosw/tech_cup/repository/TeamRepository.java
+++ Dosw-Project1/src/main/java/edu/eci/dosw/tech_cup/repository/TeamRepository.java
@@ -1,9 +1,20 @@
 package edu.eci.dosw.tech_cup.repository;
 
 import edu.eci.dosw.tech_cup.entity.Team;
+import edu.eci.dosw.tech_cup.model.enums.TeamStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Long> {
+
+    List<Team> findByTournamentId(Long tournamentId);
+
+    List<Team> findByStatus(TeamStatus status);
+
+    List<Team> findByCaptainId(Long captainId);
+
+    boolean existsByNameAndTournamentId(String name, Long tournamentId);
 }
